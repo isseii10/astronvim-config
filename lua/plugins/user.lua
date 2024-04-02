9f213c3c5c1e4112cb977e999c25215ffc71dcb4
isseii10 <t1l.g2.o11g@gmail.com>
2024-04-02 21:36:44 +0900

    enable plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -40,7 +38,7 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = true },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
